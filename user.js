bd1c002c5dca944937b6a0eba432ffdf7a2b9bdb
Huan <ngochuancntt4@gmail.com>
2018-06-03 10:14:05 +0700

    F

---
+++ user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const faker = require('faker');
+
+mongoose.connect('mongodb://admin:khoapham123@ds147030.mlab.com:47030/user1205')
+// mongoose.connect('mongodb://abc:123@ds235850.mlab.com:35850/lengochuan')
+.then(() => console.log('Database connected'))
+.catch(error => console.log('Cannot connect database', error));
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, trim: true, unique: true },
+    name: { type: String, required: true, trim: true },
+    password: { type: String, required: true, trim: true },
+    avatar: { type: String, required: true, default: faker.internet.avatar }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
